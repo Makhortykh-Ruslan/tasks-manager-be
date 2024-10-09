ac628f8b4947c5f83a0ed5d0a15fe39f874898c3
Makhortykh-Ruslan <59147778+Makhortykh-Ruslan@users.noreply.github.com>
2024-10-09 23:03:25 +0300

    add fix

--- src/utils/cors-options.ts
+++ src/utils/cors-options.ts
@@ -1,7 +1,7 @@
 export const corsOptions = {
   origin: [
     'http://localhost:4200',
-    'https://makhortykh-ruslan.github.io/tasks-manager-fe/',
+    'https://makhortykh-ruslan.github.io/tasks-manager-fe',
   ],
   optionsSuccessStatus: 200,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
